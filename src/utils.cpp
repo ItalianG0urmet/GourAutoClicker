dbb0b4423159ba64d3f85c3e6a79652ca2c61400
ItalianG0urmet <110063472+ItalianG0urmet@users.noreply.github.com>
2024-08-19 21:54:16 +0200

    Update utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -21,10 +21,10 @@ void Utils::sendMainScreen() {
     std::cout << std::left << std::setw(50) << "Welcome to GourClicker++" << std::endl;
     std::cout << std::string(50, '-') << std::endl;
 
-    std::cout << "Press <del + 1> for change the max\n" << std::endl;
+    std::cout << "Press <del + 1> for change the bypass range\n" << std::endl;
 
     std::cout << "[*] Is Enabled: " << (isEnabledLeft ? "Yes" : "No") << std::endl;
-    std::cout << "[*] Max Mouse Clicks: " << max << std::endl;
+    std::cout << "[*] Bypass range: " << max << std::endl;
 
     std::cout << std::string(50, '=') << std::endl;
 }
